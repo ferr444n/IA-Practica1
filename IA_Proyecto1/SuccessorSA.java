e95de252bde8a163bd75398091318a778e35d90d
roger.buchaca <roger.buchaca@estudiantat.upc.edu>
2026-03-26 01:50:29 +0100

    SIMULATED ANELING REVISAT

--- IA_Proyecto1/SuccessorSA.java
+++ IA_Proyecto1/SuccessorSA.java
@@ -9,50 +9,37 @@ import java.util.*;
 
 public class SuccessorSA implements SuccessorFunction {
 
-    /** Necessitem utilitzar la vuncio random 
-     * per a escollir la operacio que utilitzarem 
-     * de manera aleatoria
-     * 
-     * OJO: Simulated annealing nomes genera UN succesor de forma aleatoria!!!
-     */
     private Random rand = new Random();
 
     public List getSuccessors(Object state) {
 
-        /** Fem una copia del estat */
         RescueStates s = (RescueStates) state;
         RescueStates newState = new RescueStates(s); 
 
         int numHelis = s.getNumHelicopters();
 
-        /** S'escolleig un operador aleatori. */
         int operador = rand.nextInt(2);
         if (operador == 0) { 
-            /** S'intercanvien de forma aleatoria */
+            /** S'ESCOLLEIXEN ALEATORIAMENT */
             int h1 = rand.nextInt(numHelis);
             int h2 = rand.nextInt(numHelis);
 
-            if (s.getGrupsHelicopter(h1).size() > 0 &&
-                s.getGrupsHelicopter(h2).size() > 0) {
+            if (s.getGrupsHelicopter(h1).size() > 0 && s.getGrupsHelicopter(h2).size() > 0) {
                 int p1 = rand.nextInt(s.getGrupsHelicopter(h1).size());
                 int p2 = rand.nextInt(s.getGrupsHelicopter(h2).size());
                 newState.swap(h1, p1, h2, p2);
             }
 
         } else {
-            /** De nou, s'agafen de forma aleatoria 
-             */
+            /**S'ESCOLLEIXEN ALEATORIAMENT */
             int h1 = rand.nextInt(numHelis);
             int h2 = rand.nextInt(numHelis);
 
             if (s.getGrupsHelicopter(h1).size() > 0) {
-
                 int p1 = rand.nextInt(s.getGrupsHelicopter(h1).size());
                 int p2 = rand.nextInt(s.getGrupsHelicopter(h2).size() + 1);
-
                 newState.move(h1, p1, h2, p2);
             }
-
         }
 
         List<Successor> ret = new ArrayList<>();
